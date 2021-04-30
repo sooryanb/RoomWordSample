1b337093107fb9be47480cfb5019e7e8757366fa
sooryanarayanan <sooryanarayanan@qburst.com>
2021-04-30 16:03:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/Word.kt
@@ -0,0 +1,12 @@
+package com.example.roomwordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "word_table")
+class Word(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey @ColumnInfo(name = "word") val word: String
+)
